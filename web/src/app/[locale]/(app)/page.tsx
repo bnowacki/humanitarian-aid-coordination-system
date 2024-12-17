4add444982f09baebed33f2d5445a0a321c64a43
Condotierro <davidosx09@gmail.com>
2024-12-17 01:07:24 +0100

    Aid org mocks

--- web/src/app/[locale]/(app)/page.tsx
+++ web/src/app/[locale]/(app)/page.tsx
@@ -1,13 +1,16 @@
-import { Heading, Stack } from '@chakra-ui/react'
-import { getTranslations } from 'next-intl/server'
+﻿import { Heading, Stack } from '@chakra-ui/react';
+import { getTranslations } from 'next-intl/server';
+import AdminAidOrganizations from '../admin/aid-organizations/page'; // Import the AdminAidOrganizations component
 
 export default async function Index() {
-  const t = await getTranslations('home')
+  const t = await getTranslations('home');
 
   return (
     <Stack flex={1} w="full" gap={20} textAlign="center">
       <Heading>{t('title')}</Heading>
+      <h1>hello buła</h1>
       <Heading>TODO: List of events here</Heading>
+      <AdminAidOrganizations /> 
     </Stack>
-  )
+  );
 }
